6a06e46b9997a0db80d1e418e49a236fc7d2bfa7
jafilippini <julian_filippini@hotmail.com>
2020-06-11 16:28:48 -0300

    Add version 5

--- version/app.js
+++ version/app.js
@@ -6,7 +6,7 @@ var response;
 app.get('/', function (req, res) {
 
     // response = 'This is version 1 of the app.' + '\n';
-    response = 'Hello World!" Mensaje desde el universo Kubernetiano.' + '\n' + 'Hola amigos humanoides Luteralianos, Buuubble los espera.....súbanse a este tren.' + '\n';
+    response = '"Hello Luterals World!"--Mensaje desde el universo Kubernetiano.--' + '\n' + 'Hola amigos humanoides Luteralianos, Buuubble los espera.....súbanse a este tren.' + '\n';
 
     //send the response to the client
     res.send(response);
